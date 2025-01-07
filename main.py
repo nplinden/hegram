e2bc3d05703b3f572960c860e29983b212e6500e
Nicolas Linden <57541749+nplinden@users.noreply.github.com>
2025-01-07 21:21:03 +0100

    main test

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from hegram.app import app
 import sys
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=7777, dev_tools_hot_reload=True)
+    print(sys.argv)
+    # app.run_server(debug=True, port=7777, dev_tools_hot_reload=True)
