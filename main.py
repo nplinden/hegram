0a7f02b3a3c57b3f94a8e2675b9142c1b72f3435
Nicolas Linden <57541749+nplinden@users.noreply.github.com>
2025-01-07 19:14:23 +0100

    minor main changes

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from hegram.occurences import occurences
+from hegram.definitions import definitions
 from hegram.app import app
+import sys
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=7777, dev_tools_hot_reload=True)
+    if len(sys.argv) == 1:
+        app.run_server(debug=True, port=7777, dev_tools_hot_reload=True)
