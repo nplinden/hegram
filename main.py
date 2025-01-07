d01a9b4da88eda17aac2e5122cef8ade889fa722
Nicolas Linden <57541749+nplinden@users.noreply.github.com>
2025-01-07 21:40:13 +0100

    fix

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ from hegram.app import app
 import sys
 
 if __name__ == '__main__':
-    print(sys.argv)
-    # app.run_server(debug=True, port=7777, dev_tools_hot_reload=True)
+    app.run_server(debug=True, port=7777, dev_tools_hot_reload=True)
